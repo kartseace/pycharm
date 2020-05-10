814f8acd7a8bf68d5db358cd9f5d0840563c2e6d
Karma Lama <kartseace@MacBook-Air.local>
2020-05-10 15:34:24 +0545

    first commit

---
+++ DjangoStuff/javascript/example.js
@@ -0,0 +1,5 @@
+alert("Welcome to your bank");
+
+var deposit = prompt("How much would lke to deposit today?")
+alert("You've deposited: " + deposit)
+console.log("You are a cool person")
---
+++ DjangoStuff/javascript/p3.js
@@ -0,0 +1,6 @@
+
+var number=prompt("Enter pound to conver into kilograms")
+var kg=number*0.454
+alert("The converted weight is: " + kg)
+
+console.log("conversion completed")
